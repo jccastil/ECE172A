ce058f17920dc32fed927676946f1a1e888ba1ff
Juan Castillo <jccastil@ucsd.edu>
2022-03-08 14:50:38 -0800

    Fixed NormGrayHistogram to take in any input that is loaded globally.

--- HW3/HW3_1.py
+++ HW3/HW3_1.py
@@ -1,34 +1,40 @@
 #Test push to github
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import cv2
 import sys
 
 np.set_printoptions(threshold=sys.maxsize)
 
+#read forest.jpg
+forestjpg = cv2.imread(r"C:\Users\juanc\Desktop\Winter 2022\ECE 172\Github\Homework\ECE172A\HW3\forest.jpg")
+#read template_old.jpg
+template_old = cv2.imread(r"C:\Users\juanc\Desktop\Winter 2022\ECE 172\Github\Homework\ECE172A\HW3\template_old.jpeg")
+
+
 
 #Create function computeNormGrayHistogram
-def computeNormGrayHistogram():
+def computeNormGrayHistogram(img):
     #load the image and display it BGR
-    img1 = cv2.imread(r"C:\Users\juanc\Desktop\Winter 2022\ECE 172\Github\Homework\ECE172A\HW3\forest.jpg")
-    cv2.imshow('test BGR',img1)
+    # img1 = cv2.imread(r"C:\Users\juanc\Desktop\Winter 2022\ECE 172\Github\Homework\ECE172A\HW3\forest.jpg")
+    cv2.imshow('test BGR', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     #Convert image from BGR to RGB
-    img1rgb = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
-    cv2.imshow('test RGB',img1rgb)
+    bgr2rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    cv2.imshow('test RGB',bgr2rgb)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     #Converting the image to gray
-    grayimg1 = cv2.cvtColor(img1rgb, cv2.COLOR_RGB2GRAY)
-    cv2.imshow('test GRAY',grayimg1)
+    imggray = cv2.cvtColor(bgr2rgb, cv2.COLOR_RGB2GRAY)
+    cv2.imshow('test GRAY',imggray)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
     #Reshape the image (500x750) into a vector of 375000 elements
-    grayimg1 = np.reshape(grayimg1,-1)
+    imggray = np.reshape(imggray,-1)
 
     #ThrowawayCode***********************************************
     #Create a 32 length vector to hold counts of intensity values
@@ -48,13 +54,23 @@ def computeNormGrayHistogram():
     # print("bins32 is: ",bins32)
     # print("bins32 len is: ", len(bins32))
     #Create an array that shows which bin number the corresponding value in the index of grayimg1 belongs to
-    bin_indices = np.digitize(grayimg1, bins32)
+    bin_indices = np.digitize(imggray, bins32)
     counts = np.bincount(bin_indices)
     #code on next line was necessary in order to see the true counts in each of the bins
     np.set_printoptions(threshold=sys.maxsize)
     print("counts is: ", counts)
     print("counts len: ",len(counts))
     print("sum of counts: ",np.sum(counts))
+    #normalize the bin counts
+    counts = counts/(np.sum(counts))
+    print("normalized counts: ", counts)
+    print("new sum of counts: ", np.sum(counts))
+    #plot the histogram from the normalized bins
+    plt.hist(counts)
+    plt.title('Histogram for forest.jpg (GRAY)')
+    plt.xlabel('Normalized Intensity Values (GRAY)')
+    plt.ylabel('Occurences')
+    plt.show()
 #call the function
-computeNormGrayHistogram()
+computeNormGrayHistogram(forestjpg)
 
