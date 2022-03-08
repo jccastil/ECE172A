c5778d3ecee5e3c1006a2ce46e1f48c9805fe107
Juan Castillo <jccastil@ucsd.edu>
2022-03-08 00:20:23 -0800

    Load in the image for part 1

--- HW3/HW3_1.py
+++ HW3/HW3_1.py
@@ -1,1 +1,16 @@
 #Test push to github
+import numpy as np
+import matplotlib as plt
+import cv2
+
+
+#Create function computeNormGrayHistogram
+def computeNormGrayHistogram():
+    #load the image and display it
+    img1 = cv2.imread(r"C:\Users\juanc\Desktop\Winter 2022\ECE 172\Github\Homework\ECE172A\HW3\forest.jpg")
+    cv2.imshow('test', img1)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+#call the function
+computeNormGrayHistogram()
