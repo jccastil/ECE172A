bd1c05e4a5becebb06ebfe5de1fd832413d052d7
Juan Castillo <jccastil@ucsd.edu>
2022-03-09 01:02:39 -0800

    Created histogram for both functions using plt.bar. Need to format axes and labels as well as rearrange the RGB histogram because it is not in RGB order. Will be moving on as this is essentially formatting.

--- HW3/HW3_1.py
+++ HW3/HW3_1.py
@@ -66,11 +66,109 @@ def computeNormGrayHistogram(img):
     print("normalized counts: ", counts)
     print("new sum of counts: ", np.sum(counts))
     #plot the histogram from the normalized bins
-    plt.hist(counts)
-    plt.title('Histogram for forest.jpg (GRAY)')
-    plt.xlabel('Normalized Intensity Values (GRAY)')
-    plt.ylabel('Occurences')
+    figgray = plt.figure()
+    ax = figgray.add_axes([0, 0, 1, 1])
+    ax.bar(np.arange(1, 34, 1), counts)
     plt.show()
-#call the function
+
+
+
+
+    # #OLD*******************************
+    # plt.hist(counts)
+    # plt.title('Histogram for forest.jpg (GRAY)')
+    # plt.xlabel('Normalized Intensity Values (GRAY)')
+    # plt.ylabel('Occurences')
+    # # plt.xticks(np.arange(0, .05,.001))
+    # plt.show()
+    # #may need to fix plot so that x axis is bin number
+    # #in histogram you are currently displaying "2 times normalized bin count + 4 times normalized bin count etc = 1
+    # # OLD*******************************
+def computeNormRGBHistogram(img):
+    # load the image and display it BGR
+    cv2.imshow('test BGR', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    #obtain R,G,B channels| Note: will be leaving image in BGR (B==0, G==1, R==2)
+    redchannel = img[:,:,2]
+    greenchannel = img[:, :, 1]
+    bluechannel = img[:, :, 0]
+
+    #test save the images
+    #cv2.imwrite(r"C:\Users\juanc\Desktop\Winter 2022\ECE 172\Github\Homework\ECE172A\HW3\frstred.jpg",redchannel)
+
+    #This is strictly for visualizing each color channel******************************
+    #make B,G == 0 to visualize red image
+    redimg = np.zeros(img.shape)
+    redimg[:,:,2] = redchannel
+    #cv2.imwrite(r"C:\Users\juanc\Desktop\Winter 2022\ECE 172\Github\Homework\ECE172A\HW3\frstredvis.jpg",redimg)
+    # make B,R == 0 to visualize green image
+    greenimg = np.zeros(img.shape)
+    greenimg[:, :, 1] = greenchannel
+    #cv2.imwrite(r"C:\Users\juanc\Desktop\Winter 2022\ECE 172\Github\Homework\ECE172A\HW3\frstgreenvis.jpg", greenimg)
+    # make G,R == 0 to visualize blue image
+    blueimg = np.zeros(img.shape)
+    blueimg[:, :, 0] = bluechannel
+    #cv2.imwrite(r"C:\Users\juanc\Desktop\Winter 2022\ECE 172\Github\Homework\ECE172A\HW3\frstbluevis.jpg", blueimg)
+    #This is strictly for visualizing each color channel******************************
+
+    #bins process RED
+    redchannel = np.reshape(redchannel,-1)
+    redbins = np.array(range(0,255,8))
+    redbin_ind = np.digitize(redchannel, redbins)
+    redcounts = np.bincount(redbin_ind)
+    # code on next line was necessary in order to see the true counts in each of the bins
+    np.set_printoptions(threshold=sys.maxsize)
+    print("redcounts is: ", redcounts)
+    print("redcounts len: ", len(redcounts))
+    print("sum of redcounts: ", np.sum(redcounts))
+
+    #bins process GREEN
+    greenchannel = np.reshape(greenchannel, -1)
+    greenbins = np.array(range(0, 255, 8))
+    greenbin_ind = np.digitize(greenchannel, greenbins)
+    greencounts = np.bincount(greenbin_ind)
+    # code on next line was necessary in order to see the true counts in each of the bins
+    np.set_printoptions(threshold=sys.maxsize)
+    print("greencounts is: ", greencounts)
+    print("greencounts len: ", len(greencounts))
+    print("sum of greencounts: ", np.sum(greencounts))
+
+    #bins process BLUE
+    bluechannel = np.reshape(bluechannel, -1)
+    bluebins = np.array(range(0, 255, 8))
+    bluebin_ind = np.digitize(bluechannel, bluebins)
+    bluecounts = np.bincount(bluebin_ind)
+    # code on next line was necessary in order to see the true counts in each of the bins
+    np.set_printoptions(threshold=sys.maxsize)
+    print("bluecounts is: ", bluecounts)
+    print("bluecounts len: ", len(bluecounts))
+    print("sum of bluecounts: ", np.sum(bluecounts))
+
+    #normalize EACH histogram
+    redcounts = redcounts/(np.sum(redcounts))
+    greencounts = greencounts/(np.sum(greencounts))
+    bluecounts = bluecounts/(np.sum(bluecounts))
+
+    #create color coded concatenated histogram in RGB order (96 elements) (will be 99 until resolved)
+    #print the size of each count
+    # print("redcountsize: ", len(redcounts))
+    # print("greencountsize: ", len(greencounts))
+    # print("bluecountsize: ", len(bluecounts))
+    #each verified to be 33, first element is always 0 (might remove later)
+
+    #concatenate the counts
+    rgbcount = np.concatenate([redcounts,greencounts,bluecounts])
+    #verified that it is 99 elements. Must resolve to be 96.
+
+    #plot the histogram of RGB channels (using plt.bar)
+    fig1 = plt.figure()
+    ax = fig1.add_axes([0,0,1,1])
+    ax.bar(np.arange(1,100,1),rgbcount)
+    plt.show()
+
+
+#call the functions
 computeNormGrayHistogram(forestjpg)
 
+computeNormRGBHistogram(forestjpg)
